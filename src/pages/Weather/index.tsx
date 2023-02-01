a4058d61615ae3ba3a3c4c08230c4efcfedcc07a
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-02-01 20:25:56 -0300

    :sparkles: feat: next days forecast

--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -1,4 +1,9 @@
-import { ArrowBendLeftUp, ArrowBendRightDown, Wind } from 'phosphor-react'
+import {
+  ArrowBendLeftUp,
+  ArrowBendRightDown,
+  CloudRain,
+  Wind,
+} from 'phosphor-react'
 import { useEffect, useState } from 'react'
 
 import {
@@ -19,8 +24,7 @@ export function Weather() {
 
   function fetchUserIp() {
     fetch(
-      `https://api.ipgeolocation.io/ipgeo?apiKey=${
-        import.meta.env.VITE_IP_GEOLOCATION_KEY
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${import.meta.env.VITE_IP_GEOLOCATION_KEY
       }`,
     )
       .then((response) => response.json())
@@ -41,8 +45,7 @@ export function Weather() {
 
   function fetchForecastData() {
     fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${
-        import.meta.env.VITE_WEATHER_API_KEY
+      `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY
       }&q=${location?.city ?? 'Sao_Paulo'}&days=7&aqi=no&alerts=yes`,
     )
       .then((response) => response.json())
@@ -68,6 +71,8 @@ export function Weather() {
             WeatherApiResponseMapper(day),
           )
 
+        tempNextDaysForecast.splice(0, 1)
+
         setCurrentDayForecast(tempCurrentDayForecast)
         setNextDaysForecastProps(tempNextDaysForecast)
       })
@@ -79,59 +84,106 @@ export function Weather() {
   }, [])
 
   return (
-    <div className="w-full h-full min-h-screen flex items-center justify-center">
-      <div className="max-w-xl bg-[#fff] flex flex-col rounded-lg p-5 gap-8">
-        <div className="flex justify-between">
-          <h3>{location?.city}</h3>
-          <div className="flex gap-2">
-            <p>{location?.country.countryCode}</p>
-            <img
-              className="h-6 rounded-lg"
-              src={location?.country.flag}
-              alt={location?.country.name}
-            />
+    <div className="w-screen h-screen min-h-screen flex flex-col items-center justify-center">
+      <div className="w-full max-w-2xl flex flex-col gap-4 p-4">
+        <div className="bg-[#fff] flex flex-col rounded-lg p-5 gap-8">
+          <div className="flex justify-between">
+            <h3>{location?.city}</h3>
+            <div className="flex gap-2">
+              <p>{location?.country.countryCode}</p>
+              <img
+                className="h-4 rounded-lg"
+                src={location?.country.flag}
+                alt={location?.country.name}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 justify-center items-center">
+            <h2 className="font-black text-5xl">
+              <span className="text-8xl">
+                {currentDayForecast?.currentTemperature}
+              </span>{' '}
+              &deg;C
+            </h2>
+            <div className="flex gap-2 justify-center items-center">
+              <span>
+                <img src={currentDayForecast?.condition?.icon} alt="" />
+              </span>
+              <p>{currentDayForecast?.condition?.text}</p>
+            </div>
+          </div>
+          <div className="flex justify-center gap-4 text-sm sm:text-base">
+            <p className="flex gap-2 items-center">
+              <span>
+                <ArrowBendRightDown size={24} />
+              </span>{' '}
+              {currentDayForecast?.minTemperatureInCelsius} &deg;C
+            </p>
+            <p className="flex gap-2 items-center">
+              <span>
+                <ArrowBendLeftUp size={24} />
+              </span>{' '}
+              {currentDayForecast?.maxTemperatureInCelsius} &deg;C
+            </p>
+            <p className="flex gap-2 items-center">
+              <span>
+                <Wind size={24} />
+              </span>
+              {currentDayForecast?.windKilometersPerHour} K/h
+            </p>
           </div>
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <h2 className="font-black text-5xl">
-            <span className="text-8xl">
-              {currentDayForecast?.currentTemperature}
-            </span>{' '}
-            &deg;C
-          </h2>
-          <div className="flex gap-2 justify-center items-center">
-            <span>
-              <img src={currentDayForecast?.condition?.icon} alt="" />
-            </span>
-            <p>{currentDayForecast?.condition?.text}</p>
-          </div>
-        </div>
-        <div className="flex justify-center gap-4">
-          <p className="flex gap-2 items-center">
-            <span>
-              <ArrowBendRightDown size={24} />
-            </span>{' '}
-            {currentDayForecast?.minTemperatureInCelsius} &deg;C
-          </p>
-          <p className="flex gap-2 items-center">
-            <span>
-              <ArrowBendLeftUp size={24} />
-            </span>{' '}
-            {currentDayForecast?.maxTemperatureInCelsius} &deg;C
-          </p>
-          <p className="flex gap-2 items-center">
-            <span>
-              <Wind size={24} />
-            </span>
-            {currentDayForecast?.windKilometersPerHour} K/h
-          </p>
+        <div className="w-full max-w-full sm:max-w-none flex grid-cols-none sm:grid sm:grid-cols-3 gap-4 overflow-x-auto snap-mandatory snap-x">
+          {nextDaysForecast.length > 0 ? (
+            nextDaysForecast.map((day) => (
+              <div
+                key={String(day.date)}
+                className="max-w-[250px] bg-[#fff] flex flex-col rounded-lg p-5 gap-8 shrink-0 snap-center sm:m-0 mb-2"
+              >
+                <div className="flex justify-between">
+                  <p>{new Intl.DateTimeFormat().format(day.date)}</p>
+                  <img
+                    className="h-6 rounded-lg"
+                    src={day.condition.icon}
+                    alt={day.condition.text}
+                    title={day.condition.text}
+                  />
+                </div>
+                <div className="flex flex-col gap-4 justify-center items-center">
+                  <h1 className="flex gap-2 items-center">
+                    <span>
+                      <ArrowBendRightDown size={24} />
+                    </span>{' '}
+                    {day.minTempInCelsius} &deg;C
+                  </h1>
+                  <h1 className="flex gap-2 items-center">
+                    <span>
+                      <ArrowBendLeftUp size={24} />
+                    </span>{' '}
+                    {day.minTempInCelsius} &deg;C
+                  </h1>
+                </div>
+                <div className="flex justify-center gap-4">
+                  <p className="flex gap-2 items-center text-sm">
+                    <span>
+                      <ArrowBendLeftUp size={14} />
+                    </span>{' '}
+                    {day.minTempInCelsius} &deg;C
+                  </p>
+                  <p className="flex gap-2 items-center text-sm">
+                    <span>
+                      <CloudRain size={14} />
+                    </span>
+                    {day.chanceOfRain} %
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>There is no next days forecast!</p>
+          )}
         </div>
       </div>
-      {nextDaysForecast.length > 0 ? (
-        nextDaysForecast.map((day): NextDaysForecastProps => <p>{day.date}</p>)
-      ) : (
-        <p>There is no information about the next 6 days!</p>
-      )}
     </div>
   )
 }
